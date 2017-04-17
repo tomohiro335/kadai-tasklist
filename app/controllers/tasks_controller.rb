4e7b5b9a9a3fdd9357726ead0cb83b56553ec2e6
Tomohiro Ohno <test@test.com>
2017-04-17 06:50:51 +0000

    generate controller

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,20 @@
+class TasksController < ApplicationController
+  def index
+    
+  end
+  
+  def show
+  end
+  
+  def create
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def delete
+  end
+end
