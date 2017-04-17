b81833e97cb66b1b644449e2c03ef92f89bf25ae
Tomohiro Ohno <test@test.com>
2017-04-17 08:06:32 +0000

    controller and views comlete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,20 +1,55 @@
 class TasksController < ApplicationController
   def index
-    
+    @tasks = Task.all
   end
   
   def show
+    @task = Task.find(params[:id])
+  end
+  
+  def new
+    @task = Task.new
   end
   
   def create
+    @task = Task.new(task_params)
+    
+    if @task.save
+      flash[:success] = 'タスクが正常に登録されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'タスクが登録されませんでした'
+      render :new
+    end
   end
   
   def edit
+    @task = Task.find(params[:id])
   end
   
   def update
+    @task = Task.find(params[:id])
+    
+    if @task.update(task_params)
+      flash[:success] = 'タスクの正常更新完了'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'タスクの更新失敗'
+      render :edit
+    end
   end
   
-  def delete
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    
+    flash[:success] = 'タスクは正常に削除されました'
+    redirect_to tasks_url
+  end
+  
+  private
+  
+  def task_params
+    params.require(:task).permit(:content)
   end
 end
