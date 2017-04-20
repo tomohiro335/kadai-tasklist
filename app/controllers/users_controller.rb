9b341c309f1d838aef1af50dafa02caa6a465d8b
Tomohiro Ohno <test@test.com>
2017-04-20 02:24:41 +0000

    deleteとcountまでOK

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @tasks = @task.tasks.order('created_at DESC').pages(params[:page])
+    @tasks = @user.tasks.order('created_at DESC').page(params[:page])
     counts @user
   end
 
